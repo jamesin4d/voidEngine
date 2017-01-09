08404a7c205ed75fc44fa5057a83fb52e01c27be
James <jdiel9674@gmail.com>
2017-01-09 17:24:57 -0500

    no message

--- src/physics/grid.lua
+++ src/physics/grid.lua
@@ -1,6 +1,7 @@
 local each = encos.each
 local floor = math.floor
 local ceil = math.ceil
+local initStep = require "src.physics.traverseStep"
 
 local function toWorld(cs,cx,cy)
   return (cx-1)*cs,(cy-1)*cs
@@ -56,6 +57,42 @@ local function getItemsInRect(grid,cl,ct,cw,ch)
   return dict
 end
 
+local function traverse(cs, x1,y1,x2,y2,f) -- (cellSize,start point,end point, function(cx,cy))
+  -- this is probably one of the more confusing pieces of code i've worked with.
+  local cx1,cy1 = toCell(cs,x1,y1)
+  local cx2,cy2 = toCell(cs,x2,y2)
+  local stepX,dx,tx = initStep(cs,cx1,x1,x2)
+  local stepY,dy,ty = initStep(cs,cy1,y1,y2)
+  local cx,cy = cx1,cy1
+
+  f(cx,cy) -- THIS is a legendary lambda function i believe.
+  while math.abs(cx-cx2)+math.abs(cy-cy2)>1 do
+    if tx<ty then
+      tx, cx = tx+dx, cx+stepX
+      f(cx,cy)
+    else
+      if tx == ty then f(cx+stepX, cy) end
+      ty, cy = ty+dy, cy+stepY
+      f(cx,cy)
+    end
+  end
+  if cx ~= cx2 or cy ~=cy2 then f(cx2,cy2) end
+end
+
+local function getCellsAlongSegment(grid,x1,y1,x2,y2)
+  local cells, len, visited = {},0,{}
+  traverse(grid.cellSize,x1,y1,x2,y2,  function(cx,cy)
+      local row = grid.rows[cy]
+      if not row then return end
+      local cell = row[cx]
+      if not cell or visited[cell] then return end
+      visited[cell] = true
+      len = len+1
+      cells[len] = cell
+    end)
+  return cells, len
+end
+
 local grid = {
   addItem = addItemToCell,
   removeItem = removeItemFromCell,
@@ -65,4 +102,4 @@ local grid = {
   toCellRect = toCellRect
 }
 
-return grid 
+return grid
